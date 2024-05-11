30034b91b7ddbef301514a4dad31178060e9f360
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-11 10:05:58 -0500

    cambios

--- proyecto/clientes/urls.py
+++ proyecto/clientes/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('clientes/', views.cliente_list, name='clienteList'),
     path('clientecreate/', csrf_exempt(views.cliente_create), name='clienteCreate'),
-    path('/clientecreate-jmeter', csrf_exempt(views.cliente_create_jmeter), name='cliente_create_jmeter'),
+    path('clientecreate-jmeter/', csrf_exempt(views.cliente_create_jmeter), name='cliente_create_jmeter'),
     path('infoAdicionalcreate/', csrf_exempt(views.informacion_adicional_create), name='infoAdicionalcreate'),
     path('', include('adicionales.urls')),
 ]
