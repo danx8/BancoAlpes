a4489df3fe4fd293719c37e328a845f8a86e579e
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-11 22:27:59 -0500

    a

--- proyecto/clientes/urls.py
+++ proyecto/clientes/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('clienteFailed/', views.cliente_list, name='clienteFailed'),
     path('clientecreate/', csrf_exempt(views.cliente_create), name='clienteCreate'),
     path('clienteCreateFailed/', csrf_exempt(views.cliente_create), name='clienteCreateFailed'),
+    path('clienteEdit/', csrf_exempt(views.cliente_edit), name='clienteEdit'),
     path('clienteEdit/<int:cliente_id>/', csrf_exempt(views.cliente_edit), name='clienteEdit'),
     path('clientecreate-jmeter/', csrf_exempt(views.cliente_create_jmeter), name='cliente_create_jmeter'),
     path('infoAdicionalcreate/', csrf_exempt(views.informacion_adicional_create), name='infoAdicionalcreate'),
