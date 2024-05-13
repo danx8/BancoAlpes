f53797ce1c6077598cad74126fa4e91d736df3c4
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-13 10:26:52 -0500

    aa

--- proyecto/proyecto/auth0backend.py
+++ proyecto/proyecto/auth0backend.py
@@ -44,6 +44,6 @@ def getRole(request):
     headers = {'authorization': 'Bearer ' + accessToken}
     resp = requests.get(url, headers=headers)
     userinfo = resp.json()
-    role = userinfo['dev-7kijsxsv483uz7nf.us.auth0.com/role']
+    role = userinfo['dev-7kijsxsv483uz7nf.us.auth0.com/roles']
     return (role)
 
