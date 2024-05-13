8e1a3e6098ffe449cbb6fe1ede34114f459ab145
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-13 11:01:28 -0500

    a

--- proyecto/clientes/views.py
+++ proyecto/clientes/views.py
@@ -19,6 +19,8 @@ def cliente_list(request):
     role = getRole(request)
     print(role)
     email = getEmail(request)
+    print(email)
+    print("hola ama")
     if role != "Administrador" and role != "Normal" and email == "thesupervisor@gmail.com":
         form = ClienteForm()
         context = {
